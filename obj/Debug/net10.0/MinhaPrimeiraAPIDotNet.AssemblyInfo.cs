ca61e488f47a184a418a733ec24244251f710b5b
Uelisson <tagots09@gmail.com>
2026-01-20 00:30:00 -0300

    CRUD completo em ASP .NET

--- obj/Debug/net10.0/MinhaPrimeiraAPIDotNet.AssemblyInfo.cs
+++ obj/Debug/net10.0/MinhaPrimeiraAPIDotNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaPrimeiraAPIDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a32bc0150f1e3a789a9f58665911e69d7bf1462")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaPrimeiraAPIDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaPrimeiraAPIDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
